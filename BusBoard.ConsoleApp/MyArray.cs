c67ddadc30e9b5ae55fdbe88376936797b5ab3d2
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-09 14:19:08 +0100

    reads json and prints 5 next buses from stop

---
+++ BusBoard.ConsoleApp/MyArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusBoard.ConsoleApp
+{
+    public class MyArray    {
+        public string vehicleId { get; set; } 
+        public string stationName { get; set; } 
+        public string lineName { get; set; } 
+        public string destinationName { get; set; } 
+        public int timeToStation { get; set; } 
+        public string currentLocation { get; set; } 
+        public string towards { get; set; } 
+        public DateTime expectedArrival { get; set; } 
+        public DateTime timeToLive { get; set; } 
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
+  partial class Program
   {
     static void Main(string[] args)
     {
       ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-      
+      Console.WriteLine("Please enter a bus stop code: ");
+      string input = Console.ReadLine();
+      List<MyArray> request = GetRequest(input);
+      PrintStationInfo(request);
+    }
+
+    private static void PrintStationInfo(List<MyArray> request)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        Console.WriteLine(request[i].lineName);
+        Console.WriteLine(request[i].towards);
+        Console.WriteLine(request[i].timeToLive.AddHours(1));
+        Console.WriteLine();
+      }
+    }
+
+    private static List<MyArray> GetRequest(string input)
+    {
       var client = new RestClient();
       client.BaseUrl = new Uri("https://api.tfl.gov.uk/");
       var request = new RestRequest();
-      request.Resource = "StopPoint/490008660N/Arrivals";
+      request.Resource = "StopPoint/"+input+"/Arrivals";
       IRestResponse response = client.Execute(request);
-      Console.WriteLine(response.Content);
-      
+      List<MyArray> myDeserializedClass = JsonConvert.DeserializeObject<List<MyArray>>(response.Content);
+      return myDeserializedClass;
     }
-    
   }
 }
